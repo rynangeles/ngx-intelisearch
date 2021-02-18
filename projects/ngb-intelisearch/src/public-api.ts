85ad954c62541f79e20ce54643e91f8f71220c2b
Ryan <rynangeles@gmail.com>
2021-02-19 07:16:35 +0800

    remove ngb-intelisearch

--- projects/ngb-intelisearch/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngb-intelisearch
- */
-
-export * from './lib/ngb-intelisearch.service';
-export * from './lib/ngb-intelisearch.component';
-export * from './lib/ngb-intelisearch.module';
